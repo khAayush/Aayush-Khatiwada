416e63759d6ab73a5c856577196ae3f66323181d
khAayush <khatiwadaprince1@gmail.com>
2024-05-29 19:21:55 +0545

    Updated fixed navigation bar

--- main.js
+++ main.js
@@ -2,6 +2,9 @@ const goTop = document.querySelector(".top-btn");
 const navBar = document.getElementsByTagName("nav")[0];
 
 window.onscroll = ()=> {
+    // changes navigation bar's class
+    navBar.classList.toggle("stick", window.scrollY > 50);
+
     // Loads the top button
     if (window.scrollY >= 700) {
         goTop.style.right = '3vw';
@@ -9,12 +12,4 @@ window.onscroll = ()=> {
         goTop.style.right = '-3vw';
     }
 
-    // changes navigation bar's property
-    if (window.scrollY > 50) {
-        navBar.style.padding = '0.5vw 3vw'
-        navBar.style.background = 'gray';
-    } else {
-        navBar.style.padding = '2vw 3vw'
-        navBar.style.background = 'transparent';
-    }
 }
