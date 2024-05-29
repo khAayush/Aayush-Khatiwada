7111d67e3b8b23fe0b88bf02535845b3a94e2188
khAayush <khatiwadaprince1@gmail.com>
2024-05-29 18:35:20 +0545

    Created fixed navigation bar

--- main.js
+++ main.js
@@ -1,9 +1,20 @@
 const goTop = document.querySelector(".top-btn");
+const navBar = document.getElementsByTagName("nav")[0];
 
 window.onscroll = ()=> {
+    // Loads the top button
     if (window.scrollY >= 700) {
         goTop.style.right = '3vw';
     } else {
         goTop.style.right = '-3vw';
+    }
+
+    // changes navigation bar's property
+    if (window.scrollY > 50) {
+        navBar.style.padding = '0.5vw 3vw'
+        navBar.style.background = 'gray';
+    } else {
+        navBar.style.padding = '2vw 3vw'
+        navBar.style.background = 'transparent';
     }
 }
